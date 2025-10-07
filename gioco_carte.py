f10e3734715ef15585e0a170181208d12684e361
Lety-more <letizia.moretti@campusdavinci.edu.it>
2025-10-07 11:35:48 +0200

    fit verzion 1.0.0

--- gioco_carte.py
+++ gioco_carte.py
@@ -1,28 +1,15 @@
-"""
-1) estrare 3 carte da un mazzo detto anche lista
- [1,2,3,4,5,6,7,8,9,10,J,Q,K]
-2) calcolare punteggio usando dizionario associare il suo vallore
-"""
+#estrarre 3 carte da un mazzo [1,2,3,4,5,6,7,8,9,10,J,Q,K]
+#calcolare punteggio usando usando il dizionario e associare carta il suo valore
+
 import random
 
+somma=0
 mazzo=[1,2,3,4,5,6,7,8,9,10,"J","Q","K"]
 
-#carte_estratte=random.sample(mazzo,3)
-
-# oppure questo pero ne estri solo una:
-indice = random.randint(1,13)
-carta_estratta=mazzo[indice] 
-
-
-bj={"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"J":10,"Q":10,"K":10}
-
-somma=0
-
-
+bj={1:1,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10,"J":10,"Q":10,"K":10}
 for i in range(0,3):
-    indice=random.randint(1,13)
-    carta_estratta=mazzo[indice]
-    valore=bj[carta_estratta]
+    indice = random.randint(1,13)
+    carta_estratta = mazzo[indice]
+    valore = bj[carta_estratta]
     somma=somma+valore
-
 
