b59e3f1ec9dd26879d022638218e7842033cb7d5
Lety-more <letizia.moretti@campusdavinci.edu.it>
2025-10-14 11:26:52 +0200

    fif verzion 1.0.0
    acisa masima scorere ascisa delle x e savare lindice e metterlo nel ordinata fare print il numero

--- punti_cartesiani.py
+++ punti_cartesiani.py
@@ -1,21 +1,33 @@
 #dato un inzieme di 20 piunti su un piano cartesiamo,
 #random compresi nell'intervallo 0,10. Calcolare il
 #punto con ascissa massima e il punto con ordinata minima
+
 import random
 
 x=[]
 y=[]
-
 for i in range(0,20):
     y.append(random.randint(0,10))
     x.append(random.randint(0,10))
+#acisa masima
+#scorere lascisa delle x e savare lindice
+#fare print il numero
+ind_mas=0
+masimo=x[0]
+for i in range(0,20):
+    if x[i]>masimo:
+        masimo=x[i]
+        ind_mas=i
+print(masimo,y[ind_mas])
 
 punti_cartesiano=[]
 for i in range(0,20):
     punto=(random.randint(0,10),random.randint(0,10))
     punti_cartesiano.append(punto)
-    
+
+
 # come accedere alla x del primo numero
 print(punti_cartesiano[0][0])
 # come accedere alla y del primo numero
 print(punti_cartesiano[0][1])
+
